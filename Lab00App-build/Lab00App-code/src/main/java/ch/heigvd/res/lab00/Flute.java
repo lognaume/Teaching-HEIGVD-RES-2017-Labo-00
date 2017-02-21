d276a264d994428839ab554eabf2eb4ede42f497
Guillaume Milani <guillaume.milani@heig-vd.ch>
2017-02-21 10:46:58 +0000

    Implementation of IInstrument, Trumpet and Flute according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author lognaume
+ */
+public class Flute implements IInstrument {
+
+    @Override
+    public String play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getSoundVolume() {
+        return 5;
+    }
+
+    @Override
+    public String getColor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
